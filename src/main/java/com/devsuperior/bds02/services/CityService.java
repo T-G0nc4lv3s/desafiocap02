43a4c744d1348cea15cbf6e4a8ba31d68fa87a94
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-08-08 17:17:00 -0300

    ControllerExceptionHandler and StandardError

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -3,7 +3,9 @@ package com.devsuperior.bds02.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CityService {
@@ -30,5 +34,18 @@ public class CityService {
 		city.setName(dto.getName());
 		city = repository.save(city);
 		return new CityDTO(city);
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(ConstraintViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id does not exist " + id);
+		}
 	}
 }
